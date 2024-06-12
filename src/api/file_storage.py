bdbe7a5e448da951779f1b3e3faea206b5da3d26
theandrunique <theandrunique@gmail.com>
2024-06-12 13:08:48 +0300

    Add an error handler in case the file does not have a Content-Type header with mime type

--- src/api/file_storage.py
+++ src/api/file_storage.py
@@ -1,6 +1,6 @@
 from uuid import uuid4
 
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, HTTPException, UploadFile
 
 from src.schemas import FileUploadedResponse
 from src.services.s3_client import S3Session
@@ -14,6 +14,9 @@ async def upload_file(files: list[UploadFile]) -> FileUploadedResponse:
     uploaded_files = {}
 
     for file in files:
+        if file.content_type is None:
+            raise HTTPException(status_code=400, detail="File content type is missing.")
+
         async with S3Session().client() as client:
             key = f"{get_file_category(file.content_type)}/{uuid4()}-{file.filename}"
             result = await client.put(key, await file.read())
